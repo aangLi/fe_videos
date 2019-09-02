1026152921d06bf1d68d282f7fc585b51161b89b
aangLi <517249177@qq.com>
2019-09-02 10:00:53 +0800

    原型对象

--- js/instanceof/index.js
+++ js/instanceof/index.js
@@ -2,10 +2,21 @@
 
 function instance_of(L, R) {
     var O = R.prototype // 构造函数的原型对象
-    if (L.__proto__ === O) {
-        return true;
-    } else {
-        return false;
+    // if (L.__proto__ === O) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    L = L.__proto__;
+    while (true) {
+        if (L === null) {
+            return false;
+        }
+        if (L === O) {
+            return true;
+        }
+        L = L.__proto__;
     }
 }
 
@@ -16,7 +27,7 @@ function Animal(name) {
 
 // 原型对象的设置
 Animal.prototype = {
-    caonRun: function() {
+    caonRun: function () {
         console.log('it can run');
     }
 }
@@ -27,3 +38,18 @@ console.log(dog instanceof Animal);
 
 // Animal.prototype
 // dog.__proto__
+
+function Cat() {
+    this.speak = '喵';
+}
+
+// 原型对象
+Cat.prototype = new Animal('miao');
+// 原型对象的constructor 属性， 谁是原型对象的构造函数？
+Cat.prototype.constructor = Cat;
+
+const garfield = new Cat('加菲猫');
+// js 对象会沿着原型链， 查找 继承
+
+console.log(instance_of(garfield, Animal));
+console.log(garfield.__proto__.__proto__);
